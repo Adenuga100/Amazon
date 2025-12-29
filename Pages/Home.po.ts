5c54a0fc2032664564170e1277bae7d638058ebb
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2025-12-29 04:36:25 -0800

    wait

--- Pages/Home.po.ts
+++ Pages/Home.po.ts
@@ -3,10 +3,18 @@ import { Locator, Page } from "@playwright/test";
 
 export class HomePage {
 
-      static async searchInputAs( page: Page, product: string) : Promise<void> {
-       return await page.locator('#twotabsearchtextbox').fill(product);
+      static async searchInputAs( page: Page, product: string,) : Promise<void> {
+        let searchInput = page.locator('#twotabsearchtextbox',);
+        return await searchInput.waitFor({ state: 'visible', timeout: 10000 }).then( async () => {
+       return await searchInput.fill(product); });
       }
+      
 
+      // .then(async () => {
+      //     return await searchInput.fill(product);
+      //   }
+      // await page.waitForSelector('#twotabsearchtextbox', { state: 'visible' });
+      // return await page.locator('#twotabsearchtextbox').fill(product);
     //   static async pressEnter( page: Page) : Promise<void> {
     //    return await page.locator('#twotabsearchtextbox').press('Enter');
     //  }
